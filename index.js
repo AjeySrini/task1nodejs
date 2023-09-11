120d9f8ca0c8b05f113011bf1c19e6e14542c481
Ajey <ajeysrini@gmail.com>
2023-09-11 20:52:25 +0530

    fix

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import express, { json } from 'express';
-import { writeFile, readdir } from 'fs';
-
-
+const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000; 
 
-app.use(json());
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
